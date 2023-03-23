895e0b2b02edb506b0a667d0845f7f26deeeea19
Jorge Andujo <jandujo@interaxis.com.mx>
2023-03-23 00:02:53 -0700

    cambio url api somee

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const formatosValidosFotoPerfil = ["jpg", "png", "gif", "jpeg", "bmp", "svg"];
 
 export const config = {
-    API_URL: "https://localhost:7073/api/",
-    // API_URL: "http://inventariosm.somee.com/api/"
+    // API_URL: "https://localhost:7073/api/",
+    API_URL: "https://www.inventariosm.somee.com/api/"
 }
