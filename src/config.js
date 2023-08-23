2a2c6d8111121c6a1fca67fc34e324c18806e133
Jorge Andujo <jandujo@interaxis.com.mx>
2023-08-22 18:55:43 -0700

    modificar url api

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const formatosValidosFotoPerfil = ["jpg", "png", "gif", "jpeg", "bmp", "s
 
 export const config = {
     // API_URL: "https://localhost:7073/api/",
-    API_URL: "https://www.inventariosm.somee.com/api/"
+    API_URL: "https://www.inventariosmapi.somee.com/api/"
 }
